2f5297c82d9560975c66dc3cc9e3fbc821eba926
Koya Seth <30855417+KuyaKoya@users.noreply.github.com>
2022-07-04 14:03:48 +0800

    Added folder structure Implemented Custom Classes

---
+++ lib/posts/screens/home.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  @override
+  State<HomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+  final Map<String, Color> colors = {
+    'purple': Colors.purple,
+    'blue': Colors.blue,
+    'yellow': Colors.yellow,
+    'pink': Colors.pink,
+    'teal': Colors.teal,
+    'orange': Colors.orange
+  };
+
+  Color? selectedColor;
+
+  void _setColor(String colorName, Color color) {
+    setState(() {
+      selectedColor = color;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: selectedColor,
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (var entry in colors.entries)
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: entry.value,
+                      minimumSize: const Size(300, 60),
+                    ),
+                    child: const Text(''),
+                    onPressed: () => _setColor(entry.key, entry.value),
+                  ),
+                )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
