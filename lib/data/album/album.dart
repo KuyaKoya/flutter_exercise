17a3a7b361a1bc8fd42392e22e5991e636bc6dd8
schel <robertjschelling@su.edu.ph>
2022-07-05 10:18:01 +0800

    Edited data classes to accomodate Json

---
+++ lib/data/album/album.dart
@@ -0,0 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'album.g.dart';
+
+@JsonSerializable()
+class Album {
+  final String userId;
+  final int id;
+  final String title;
+
+  Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) => _$AlbumFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AlbumToJson(this);
+}
---
+++ lib/data/comment/comment.dart
@@ -0,0 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'comment.g.dart';
+
+@JsonSerializable()
+class Comment {
+  final String postId;
+  final String id;
+  final String name;
+  final String email;
+  final String body;
+
+  Comment({
+    required this.postId,
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.body,
+  });
+
+  factory Comment.fromJson(Map<String, dynamic> json) =>
+      _$CommentFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CommentToJson(this);
+}
