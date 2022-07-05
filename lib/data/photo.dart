cdfd2abe954530c5381da215431cd7149de5f09b
schel <robertjschelling@su.edu.ph>
2022-07-05 08:37:32 +0800

    Added data classes

---
+++ lib/data/photo.dart
@@ -0,0 +1,15 @@
+class Photo {
+  final String? albumId;
+  final String? id;
+  final String? title;
+  final String? url;
+  final String? thumbnailUrl;
+
+  Photo({
+    this.albumId,
+    this.id,
+    this.title,
+    this.url,
+    this.thumbnailUrl,
+  });
+}
