645a52e928cd9fa31ae4103f9d03e4741b19e11b
Anthony Heitzeberg <anthony.heitzeberg@mllrdev.com>
2022-07-14 10:52:24 +0800

    Revert "add bloc state managemenet, and post bloc"
    This reverts commit 16373e47b49f2846941261c23367b9526360be9a.

--- lib/presentation/states/post/post_bloc.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-
-import 'post_event.dart';
-import 'post_state.dart';
-
-class PostBloc extends Bloc<PostEvent, PostState> {
-  PostBloc() : super(const PostState.initial());
-}
