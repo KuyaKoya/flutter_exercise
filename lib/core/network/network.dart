46c7d90a072e51e929cf36993f8b676be4f92d45
schel <robertjschelling@su.edu.ph>
2022-07-14 10:06:25 +0800

    added core files

---
+++ lib/core/network/network.dart
@@ -0,0 +1,50 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_exercise/core/network/logging.dart';
+import '../utils.dart';
+
+class NetworkManager {
+  final Dio dio;
+
+  const NetworkManager._(this.dio);
+
+  factory NetworkManager() {
+    final dio = Dio();
+
+    dio.interceptors.add(Logging());
+    return NetworkManager._(dio);
+  }
+
+  Future<Response<T>> request<T>(
+    RequestMethod method,
+    String url, {
+    data,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? queryParameters,
+  }) {
+    return dio.request(
+      url,
+      data: data,
+      queryParameters: queryParameters,
+      options: Options(
+        method: method.value,
+        headers: headers,
+      ),
+    );
+  }
+}
+
+enum RequestMethod {
+  get,
+  head,
+  post,
+  put,
+  delete,
+  connect,
+  options,
+  trace,
+  patch,
+}
+
+extension RequestMethodX on RequestMethod {
+  String get value => getEnumValue(this).toUpperCase();
+}
