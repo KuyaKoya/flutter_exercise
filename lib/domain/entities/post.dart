41a1fd034703cdf26d25e72214d8810fd18937d0
Anthony Heitzeberg <anthony.heitzeberg@mllrdev.com>
2022-07-14 10:53:14 +0800

    add bloc state managemenet, and post bloc

---
+++ lib/domain/entities/post.dart
@@ -0,0 +1,13 @@
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+}
