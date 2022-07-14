70427b34bbf3317e91190ca95250f6d411b0c3b6
Koya Seth <30855417+KuyaKoya@users.noreply.github.com>
2022-07-14 15:37:32 +0800

    add post widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_exercise/data/source/remote/jsonPlaceholderAPI/models/post_request.dart';
+import 'package:flutter_exercise/presentation/ui/screens/post/post_list.dart';
 
 void main() {
   PostRequest().getPosts();
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: Main(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -17,27 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const PostList(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
   }
 }
