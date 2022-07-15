7251501c59fc83a1ddf3601919df7f07157f2f3b
Anthony Heitzeberg <anthony.heitzeberg@mllrdev.com>
2022-07-15 15:54:14 +0800

    add post and album count

--- lib/presentation/ui/screens/profile/profile_screen.dart
+++ lib/presentation/ui/screens/profile/profile_screen.dart
@@ -37,71 +37,71 @@ class _ProfileScreenState extends State<ProfileScreen> {
         }
 
         return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.blue, Colors.blue.shade200],
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-                stops: const [0.5, 0.9],
-              ),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: const <Widget>[
-                    CircleAvatar(
-                      backgroundColor: Colors.white70,
-                      minRadius: 60.0,
-                      child: CircleAvatar(
-                        radius: 55.0,
-                        backgroundImage: NetworkImage(
-                            'https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png'),
+          body: ListView(
+            children: <Widget>[
+              Container(
+                height: 250,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.blue, Colors.blue.shade200],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    stops: const [0.5, 0.9],
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const <Widget>[
+                        CircleAvatar(
+                          backgroundColor: Colors.white70,
+                          minRadius: 60.0,
+                          child: CircleAvatar(
+                            radius: 55.0,
+                            backgroundImage: NetworkImage(
+                                'https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png'),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      'John Smith',
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Text(
+                      'Flutter Developer',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
                       ),
                     ),
                   ],
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text(
-                  'John Smith',
-                  style: TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                const Text(
-                  'Flutter Developer',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Column(
-            children: <Widget>[
-              createInfoTile('Email', 'john.smith@mllrdev.com'),
-              const Divider(),
-              createInfoTile('Company', 'Romaguera-Crona'),
-              const Divider(),
-              createInfoTile('Phone', '92998-3874'),
-              const Divider(),
-              createInfoTile('Website', 'hildegard.org'),
+              ),
+              Column(
+                children: <Widget>[
+                  createInfoTile('Email', 'john.smith@mllrdev.com'),
+                  const Divider(),
+                  createInfoTile('Company', 'Romaguera-Crona'),
+                  const Divider(),
+                  createInfoTile('Phone', '92998-3874'),
+                  const Divider(),
+                  createInfoTile('Website', 'hildegard.org'),
+                ],
+              )
             ],
-          )
-        ],
-      ),
-    );
+          ),
+        );
       },
     );
   }
@@ -124,4 +124,57 @@ Widget createInfoTile(title, subTitle) {
       ),
     ),
   );
+}
+
+Widget createPostAlbumCount() {
+  return Row(children: <Widget>[
+    Expanded(
+      child: Container(
+        color: Colors.blueAccent.shade200,
+        child: const ListTile(
+          title: Text(
+            '5000',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              color: Colors.white,
+            ),
+          ),
+          subtitle: Text(
+            'Posts',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white70,
+            ),
+          ),
+        ),
+      ),
+    ),
+    Expanded(
+      child: Container(
+        color: Colors.blueAccent.shade200,
+        child: const ListTile(
+          title: Text(
+            '5000',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              color: Colors.white,
+            ),
+          ),
+          subtitle: Text(
+            'Albums',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white70,
+            ),
+          ),
+        ),
+      ),
+    ),
+  ]);
 }
