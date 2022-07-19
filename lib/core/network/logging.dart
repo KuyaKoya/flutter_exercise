35d4ea6c7b01db9cbc63985429c295f43fa1f3cb
schel <robertjschelling@su.edu.ph>
2022-07-19 16:22:45 +0800

    modified network logging

--- lib/core/network/logging.dart
+++ lib/core/network/logging.dart
@@ -4,14 +4,14 @@ import 'package:flutter/cupertino.dart';
 class Logging extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    debugPrint('REQUEST[${options.method}] => PATH: ${options.path}');
+    debugPrint('REQUEST[${options.method}] => PATH: ${options.uri}');
     return super.onRequest(options, handler);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
     debugPrint(
-        'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}');
+        'RESPONSE[${response.statusCode}] => DATA: ${response.data}');
     return super.onResponse(response, handler);
   }
 
